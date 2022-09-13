3f01de13a5fc0e8ac8781e14f3b436f6466c469b
razzaksr <razzaksr@gmail.com>
2022-09-13 11:13:22 +0530

    app created, first view called

---
+++ hai/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Course backups\Python\MohanGesnuDJango\ApplicationOne\hai\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
